a62af4ba452404b74e151fcfb278d307d8f011e5
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-20 16:31:07 +0200

    add modal

--- src/components/ButtonsNav.jsx
+++ src/components/ButtonsNav.jsx
@@ -7,16 +7,40 @@ import GroupAddIcon from '@material-ui/icons/GroupAdd';
 import PersonAddDisabledIcon from '@material-ui/icons/PersonAddDisabled';
 import { addUserAction } from '../store/userReducer';
 import { useDispatch, useSelector } from 'react-redux';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     root: {
         width: 500,
     },
-});
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 export default function ButtonsNav() {
     const classes = useStyles();
+
     const [value, setValue] = React.useState(0);
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const dispatch = useDispatch();
     const users = useSelector((state) => state.users.users);
@@ -43,7 +67,8 @@ export default function ButtonsNav() {
                     <BottomNavigationAction
                         label='Add User'
                         icon={<PersonAddIcon />}
-                        onClick={() => addUser(prompt())}
+                        // onClick={() => addUser(prompt())}
+                        onClick={handleOpen}
                     />
                     <BottomNavigationAction
                         label='Add Users'
@@ -54,6 +79,29 @@ export default function ButtonsNav() {
                         icon={<PersonAddDisabledIcon />}
                     />
                 </BottomNavigation>
+                <Modal
+                    aria-labelledby='transition-modal-title'
+                    aria-describedby='transition-modal-description'
+                    className={classes.modal}
+                    open={open}
+                    onClose={handleClose}
+                    closeAfterTransition
+                    BackdropComponent={Backdrop}
+                    BackdropProps={{
+                        timeout: 500,
+                    }}
+                >
+                    <Fade in={open}>
+                        <div className={classes.paper}>
+                            <h2 id='transition-modal-title'>
+                                Transition modal
+                            </h2>
+                            <p id='transition-modal-description'>
+                                react-transition-group animates me.
+                            </p>
+                        </div>
+                    </Fade>
+                </Modal>
             </div>
         </>
     );
