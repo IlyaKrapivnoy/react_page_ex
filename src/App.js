c03efec7db5a0342bd6bd26306bd333908b603df
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-20 15:24:30 +0200

    add ButtonsNav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
-import Table from './components/Table';
+// import Table from './components/Table';
+import ButtonsNav from './components/ButtonsNav';
 
 function App() {
     return (
         <div>
             <Header />
-            <Table />
+            {/* <Table /> */}
+            <ButtonsNav />
         </div>
     );
 }
