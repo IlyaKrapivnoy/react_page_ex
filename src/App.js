bde8f2a29c08957a1faa697c19bbd7058a98f55c
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-20 13:49:53 +0200

    add Header and log In / Out btn

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Header from './components/Header';
 import Table from './components/Table';
 
 function App() {
     return (
         <div className=''>
+            <Header />
             <Table />
         </div>
     );
