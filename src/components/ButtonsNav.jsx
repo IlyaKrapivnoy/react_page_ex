ea62ba63a3650f5250c57879b24ad3fb83a47272
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-11-24 18:48:59 +0200

    add validation to the form

--- src/components/ButtonsNav.jsx
+++ src/components/ButtonsNav.jsx
@@ -14,6 +14,8 @@ import Typography from '@material-ui/core/Typography';
 import { fetchUsers } from '../store/acyncActions/fetchUsers';
 import InputMask from 'react-input-mask';
 import { Formik, Form } from 'formik';
+import TextFieldWrapper from '../FormikUI/TextFieldWrapper';
+
 import * as Yup from 'yup';
 
 const useStyles = makeStyles((theme) => ({
@@ -115,28 +117,6 @@ export default function ButtonsNav() {
         //     }));
         // }
 
-        // // useremail validation
-        // let useremailReg = new RegExp('^[^s@]+@[^s@]+.[^s@]{2,}$').test(
-        //     userInfo.userEmail
-        // );
-        // if (!useremailReg) {
-        //     setErrors((state) => ({
-        //         ...state,
-        //         useremail: 'email is not valid',
-        //     }));
-        // }
-
-        // // usercity validation
-        // let usercityReg = new RegExp(
-        //     '^(?=.{3,20}$)(?:[a-zA-Zd]+(?:(?:.|-|_)[a-zA-Zd])*)+$'
-        // ).test(userInfo.userCity);
-        // if (!usercityReg) {
-        //     setErrors((state) => ({
-        //         ...state,
-        //         usercity: '3 to 20 characters limit',
-        //     }));
-        // }
-
         if (
             !(userInfo.userName,
             userInfo.userEmail,
@@ -144,9 +124,6 @@ export default function ButtonsNav() {
             userInfo.userPhone)
         ) {
             alert('Please, fill all the data');
-            // setUserInfo({
-            //     userInfo,
-            // });
             return;
         }
         dispatch(addUserAction(user));
@@ -154,7 +131,6 @@ export default function ButtonsNav() {
         setUserInfo({
             userInfo,
         });
-        // setErrors(errors);
     };
 
     return (
@@ -196,64 +172,68 @@ export default function ButtonsNav() {
                             <Typography variant='h4' gutterBottom className=''>
                                 Add User
                             </Typography>
-                            <form
-                                className={classes.form}
-                                noValidate
-                                autoComplete='off'
+                            <Formik
+                                initialValues={{ ...INITIAL_FORM_STATE }}
+                                validationSchema={FORM_VALIDATION}
+                                onSubmit={(formData) => {
+                                    console.log(formData);
+                                }}
                             >
-                                <TextField
-                                    variant='filled'
-                                    label='username'
-                                    name='userName'
-                                    value={userInfo.userName}
-                                    onChange={handleInputChange}
-                                    required
-                                    // error={errors?.username}
-                                    // helperText={errors?.username}
-                                />
-                                <TextField
-                                    variant='filled'
-                                    label='email'
-                                    name='userEmail'
-                                    value={userInfo.userEmail}
-                                    onChange={handleInputChange}
-                                    required
-                                    // error={errors?.useremail}
-                                    // helperText={errors?.useremail}
-                                />
-                                <InputMask
-                                    mask='+38 (099) 999-99-99'
-                                    value={userInfo.userPhone}
-                                    onChange={handleInputChange}
-                                >
-                                    {() => (
-                                        <TextField
-                                            variant='filled'
-                                            label='phone'
-                                            name='userPhone'
-                                            required
-                                        />
-                                    )}
-                                </InputMask>
+                                <Form className={classes.form}>
+                                    <TextFieldWrapper
+                                        variant='filled'
+                                        label='username'
+                                        name='userName'
+                                        value={userInfo.userName}
+                                        onChange={handleInputChange}
+                                        required
+                                        // error={errors?.username}
+                                        // helperText={errors?.username}
+                                    />
+                                    <TextFieldWrapper
+                                        variant='filled'
+                                        label='email'
+                                        name='userEmail'
+                                        value={userInfo.userEmail}
+                                        onChange={handleInputChange}
+                                        required
+                                        // error={errors?.useremail}
+                                        // helperText={errors?.useremail}
+                                    />
+                                    <InputMask
+                                        mask='+38 (099) 999-99-99'
+                                        value={userInfo.userPhone}
+                                        onChange={handleInputChange}
+                                    >
+                                        {() => (
+                                            <TextFieldWrapper
+                                                variant='filled'
+                                                label='phone'
+                                                name='userPhone'
+                                                required
+                                            />
+                                        )}
+                                    </InputMask>
 
-                                <TextField
-                                    variant='filled'
-                                    label='city'
-                                    name='userCity'
-                                    value={userInfo.userCity}
-                                    onChange={handleInputChange}
-                                    required
-                                    // error={errors?.usercity}
-                                    // helperText={errors?.usercity}
-                                />
-                                <Button
-                                    variant='contained'
-                                    color='primary'
-                                    onClick={(e) => addUser([userInfo])}
-                                >
-                                    Submit
-                                </Button>
-                            </form>
+                                    <TextFieldWrapper
+                                        variant='filled'
+                                        label='city'
+                                        name='userCity'
+                                        value={userInfo.userCity}
+                                        onChange={handleInputChange}
+                                        required
+                                        // error={errors?.usercity}
+                                        // helperText={errors?.usercity}
+                                    />
+                                    <Button
+                                        variant='contained'
+                                        color='primary'
+                                        onClick={(e) => addUser([userInfo])}
+                                    >
+                                        Submit
+                                    </Button>
+                                </Form>
+                            </Formik>
                         </div>
                     </Fade>
                 </Modal>
