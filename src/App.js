0c9d3e5a6c22e5fbf9f4cc408a78081c4a9b9263
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2023-02-28 10:04:40 +0100

    change code style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import './App.css';
-import Header from './components/Header';
-import Table from './components/Table';
-import ButtonsNav from './components/ButtonsNav';
+import "./App.css";
+import Header from "./components/Header";
+import Table from "./components/Table";
+import ButtonsNav from "./components/ButtonsNav";
 
-function App() {
-    return (
-        <div>
-            <Header />
-            <ButtonsNav />
-            <Table />
-        </div>
-    );
-}
+const App = () => (
+  <>
+    <Header />
+    <ButtonsNav />
+    <Table />
+  </>
+);
 
 export default App;
